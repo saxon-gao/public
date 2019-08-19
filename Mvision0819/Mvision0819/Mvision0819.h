705929ab4209e7bce0c3ab9157a6af76cce3f311
saxon-gao <xs.gao@foxmail.com>
2019-08-19 14:36:23 +0800

    Mvision0819 创建

---
+++ Mvision0819/Mvision0819/Mvision0819.h
@@ -0,0 +1,32 @@
+﻿
+// Mvision0819.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMvision0819App:
+// 有关此类的实现，请参阅 Mvision0819.cpp
+//
+
+class CMvision0819App : public CWinApp
+{
+public:
+	CMvision0819App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMvision0819App theApp;
